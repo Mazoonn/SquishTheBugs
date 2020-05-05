64496eb0ea6432e4e3205272309959cc996a9dde
SlavikMironov <61431810+SlavikMironov@users.noreply.github.com>
2020-05-05 22:16:23 +0300

    Toast

--- app/src/main/java/com/example/squishthebugs/MainActivity.java
+++ app/src/main/java/com/example/squishthebugs/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.squishthebugs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.black_bug).setOnClickListener(new
+        View.OnClickListener()
+        {
+         @Override
+        public void onClick(View v)
+         {
+             Toast.makeText(MainActivity.this,"Squish the bugs: Slava, Matan, Ashot.",Toast.LENGTH_LONG).show() ;
+         }
+        });
+
     }
+
 }
