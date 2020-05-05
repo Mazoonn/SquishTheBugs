fcbb8307ce0a0748926a46395a8cd2cd7bfa8906
Matan Nunberg <nunberg20@gmail.com>
2020-05-05 23:16:38 +0300

    init logo

--- app/src/main/java/com/example/squishthebugs/LogoActivity.java
+++ app/src/main/java/com/example/squishthebugs/LogoActivity.java
@@ -2,6 +2,7 @@ package com.example.squishthebugs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class LogoActivity extends AppCompatActivity {
@@ -10,5 +11,20 @@ public class LogoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    synchronized (this) {
+                        wait(2000);
+                        Intent intent = new Intent (LogoActivity.this,MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
