f5df9400c928ce6f98933422d08ee72cd0a4aef3
ashot <ashot@DESKTOP-LVQ20RG>
2020-05-08 10:10:47 +0300

    Main Menu

--- app/src/main/java/com/example/squishthebugs/MainActivity.java
+++ app/src/main/java/com/example/squishthebugs/MainActivity.java
@@ -40,6 +40,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);}
         });
 
+        findViewById(R.id.login_buttom).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,
+                        LoginActivity.class);
+                startActivity(intent);}
+        });
+
 
     }
 
