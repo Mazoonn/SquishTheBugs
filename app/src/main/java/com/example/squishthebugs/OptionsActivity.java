18676499ced652fc23d031c07963f571febecb69
SlavikMironov <61431810+SlavikMironov@users.noreply.github.com>
2020-06-09 16:13:11 +0300

    Options information clickable

--- app/src/main/java/com/example/squishthebugs/OptionsActivity.java
+++ app/src/main/java/com/example/squishthebugs/OptionsActivity.java
@@ -5,15 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class OptionsActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+        findViewById(R.id.information).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                TextView txtView = findViewById(R.id.text_information);
+                if (txtView.getVisibility() == View.VISIBLE)
+                    txtView.setVisibility(View.INVISIBLE);
+                else
+                    txtView.setVisibility(View.VISIBLE);
+            }
+        });
 
-        findViewById(R.id.button_return).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.button_return).setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(OptionsActivity.this,
